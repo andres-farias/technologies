bb8a70a1f16fa351a7321648e1dd3b390c1d3d8e
Andres Spirits <andres.farias@gmail.com>
2020-06-26 09:22:41 -0400

    Final solution with only one application.yml file.

---
+++ spring/microservices/ms-profiles/src/main/java/com/bluesky/technologies/springboot/profiles/MessageProperties.java
@@ -0,0 +1,29 @@
+package com.bluesky.technologies.springboot.profiles;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+@ConfigurationProperties("messages")
+public class MessageProperties {
+
+    private String message;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    @Override
+    public String toString() {
+        return "MessageProperties{" +
+                "message='" + message + '\'' +
+                '}';
+    }
+}
--- spring/microservices/ms-profiles/src/main/java/com/bluesky/technologies/springboot/profiles/ProfilesSpringBootApplication.java
+++ spring/microservices/ms-profiles/src/main/java/com/bluesky/technologies/springboot/profiles/ProfilesSpringBootApplication.java
@@ -1,6 +1,8 @@
 package com.bluesky.technologies.springboot.profiles;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,11 +10,18 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @SpringBootApplication
-public class ProfilesSpringBootApplication {
+public class ProfilesSpringBootApplication implements CommandLineRunner {
 
-    @Value("${spring.profiles.dev.message}")
+    @Value("${messages.message}")
     private String message;
 
+    @Autowired
+    private MessageProperties messageProperties;
+
+    @Override
+    public void run(String... args) {
+        System.out.println(messageProperties);
+    }
     /**
      * This class will let Spring to scan the current and underneath package.
      *
@@ -27,7 +36,7 @@ public class ProfilesSpringBootApplication {
      *
      * @return A simple text saying hello to the world!
      */
-    @RequestMapping(value = "/messanger")
+    @RequestMapping(value = "/messenger")
     public String available() {
         return message;
     }
